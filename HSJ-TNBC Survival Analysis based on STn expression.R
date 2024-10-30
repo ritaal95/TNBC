91a135f1a7e269ca00e7199bab584da66a0e1064
RALourenço <117275261+ritaal95@users.noreply.github.com>
2024-10-30 11:47:38 +0000

    Update HSJ-TNBC Survival Analysis based on STn expression.R

--- HSJ-TNBC Survival Analysis based on STn expression.R
+++ HSJ-TNBC Survival Analysis based on STn expression.R
@@ -12,7 +12,7 @@ library(survival)
 library(survminer)
 library(readxl)
 
-#Select the Directory where you can find the table with the information regarding the HSJ-TNBC cohort Table S2
+#Select the Directory where you can find the Table S2 with the information regarding the HSJ-TNBC cohort
 
 ##Preparing the data table for analysis
 TNBC_Data <- as.data.frame(read_xlsx("data/HSJ-TNBC_Data.xlsx"))
